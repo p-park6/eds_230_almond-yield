7de44767f70d3da959657295e9922724408242bc
Vanessa-Salgado <vsalgadozavaleta@umail.ucsb.edu>
2024-04-11 15:44:37 -0700

    added diagram

--- almond_yield.R
+++ almond_yield.R
@@ -8,7 +8,7 @@
 #' @examples
 almond_yield<- function(dataset) {
   
-  # pull out minimum february temperatures from each year
+  # calculate minimum temperatures in Feb from each year
   feb_tmin_c <- dataset %>%
     group_by(month, year) %>%
     filter(month == 2) %>% # select just feb
@@ -16,7 +16,7 @@ almond_yield<- function(dataset) {
     group_by() %>% 
     select(-month)# get the min
   
-  # calculate total january precipitation for each year
+  # calculate total precipitation in Jan for each year
   jan_precip_mm <- dataset %>%
     group_by(month, year) %>%
     filter(month == 1) %>% # select just jan
@@ -25,17 +25,17 @@ almond_yield<- function(dataset) {
     select(-month)# get the sum
   
   # calculate almond yield 
-  yield_anomaly <- full_join(feb_tmin_c, jan_precip_mm) %>%
-    mutate(yield_anomaly = -0.015*feb_tmin_c - 0.0046*feb_tmin_c^2 - 0.07*jan_precip_mm + 0.0043*jan_precip_mm^2 + 0.28) # calc based on equation from lobell et al. 2006
+  yield <- full_join(feb_tmin_c, jan_precip_mm) %>%
+    mutate(yield_tons = -0.015*feb_tmin_c - 0.0046*feb_tmin_c^2 - 0.07*jan_precip_mm + 0.0043*jan_precip_mm^2 + 0.28) # calc based on equation from lobell et al. 2006
   
   # calculate the min, max, and mean yield over the whole time period
-  min_yield <- min(yield_anomaly$yield_anomaly)
-  max_yield <- max(yield_anomaly$yield_anomaly)
-  mean_yield <- mean(yield_anomaly$yield_anomaly)
+  min_yield <- min(yield$yield_tons)
+  max_yield <- max(yield$yield_tons)
+  mean_yield <- mean(yield$yield_tons)
   
   # print the min, max, and mean
-  return(print(paste0("Minimum Yield:", round(min_yield, 2), "ton(s) per acre |",
-                      "Maximum Yield:", round(max_yield, 2), "ton(s) per acre |",
+  return(print(paste0("Minimum Yield:", round(min_yield, 2), "ton(s) per acre , ",
+                      "Maximum Yield:", round(max_yield, 2), "ton(s) per acre , ",
                       "Mean Yield:", round(mean_yield, 2), "ton(s) per acre")))
   
 }
