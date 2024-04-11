84387aa1e03ff1eea6ea91562aaeedd4cc303af8
Vanessa-Salgado <vsalgadozavaleta@umail.ucsb.edu>
2024-04-11 16:00:55 -0700

    fixed html

--- almond_yield.R
+++ almond_yield.R
@@ -9,7 +9,7 @@
 almond_yield<- function(dataset) {
   
   # calculate minimum temperatures in Feb from each year
-  feb_tmin_c <- dataset %>%
+  min_temp <- dataset %>%
     group_by(month, year) %>%
     filter(month == 2) %>% # select just feb
     summarise(feb_tmin_c = mean(tmin_c)) %>% 
@@ -17,7 +17,7 @@ almond_yield<- function(dataset) {
     select(-month)# get the min
   
   # calculate total precipitation in Jan for each year
-  jan_precip_mm <- dataset %>%
+  precip <- dataset %>%
     group_by(month, year) %>%
     filter(month == 1) %>% # select just jan
     summarise(jan_precip_mm = sum(precip)) %>% 
@@ -25,7 +25,7 @@ almond_yield<- function(dataset) {
     select(-month)# get the sum
   
   # calculate almond yield 
-  yield <- full_join(feb_tmin_c, jan_precip_mm) %>%
+  yield <- full_join(min_temp, precip) %>%
     mutate(yield_tons = -0.015*feb_tmin_c - 0.0046*feb_tmin_c^2 - 0.07*jan_precip_mm + 0.0043*jan_precip_mm^2 + 0.28) # calc based on equation from lobell et al. 2006
   
   # calculate the min, max, and mean yield over the whole time period
