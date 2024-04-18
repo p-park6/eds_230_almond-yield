a8d98c8958e219f8e033497b9b5daaba28e66e14
Vanessa-Salgado <vsalgadozavaleta@umail.ucsb.edu>
2024-04-17 21:32:35 -0700

    added roxygen

--- R/almond_profit_model.R
+++ R/almond_profit_model.R
@@ -1,4 +1,17 @@
-# water_usage_per_ton
+#' Almond Profit Model 
+#'
+#' @param gross_revenue 
+#' @param almond_yield 
+#' @param gallons gallons of water ; default = 9000 
+#' @param acres default = 10
+#' @param prop_of_loss proportion of yield loss ;default = 0.25
+#' @param price_of_water price of water ; default = 0.007
+#' @param price_per_ton price per ton of almonds ; defualt = 4000
+#'
+#' @return income as a gross revenue - expenses of water_price and price_of_yield_loss
+#' @export
+#'
+#' @examples
 almond_profit_model = function(gross_revenue, almond_yield, gallons = 9000, acres = 10, prop_of_loss = 0.25 ,  price_of_water = 0.007,  price_per_ton = 4000) {
   
   # water usage 
