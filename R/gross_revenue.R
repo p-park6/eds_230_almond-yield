bc7fb9d44b0e8ffc8cd8b7a77c5aba0277793a94
p-park6 <pattypark6@gmail.com>
2024-04-18 04:26:19 +0000

    fixed gross revenue column in the function

--- R/gross_revenue.R
+++ R/gross_revenue.R
@@ -1,14 +1,19 @@
-#' Title
+#' Gross Revenue for Almond yield
 #'
-#' @param almond_yield 
-#' @param price_per_ton 
+#' @param almond_yield The number of almonds obtained
+#' @param price_per_ton Price of almonds per ton
 #'
-#' @return
+#' @return The total revenue of the almonds obtained
 #' @export
 #'
 #' @examples
 gross_revenue = function(almond_yield, price_per_ton = 4000) {
-
+  #equation to find the gross revenue of almond yield
   gross_revenue = (almond_yield * 0.9) * price_per_ton
-  return(gross_revenue)
+  
+  #rename column to gross_revenue
+  gross_revenue_rename <- rename(gross_revenue, gross_revenue = yield_tons)
+  
+  #return the gross revenue of the price of almond yield
+  return(gross_revenue_rename)
 }
